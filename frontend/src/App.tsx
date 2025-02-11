d69a5eb5b9281f5303c5b251b7253673ae8a171b
David Vitas <david.vitas@hotmail.com>
2025-02-11 10:51:39 +0100

    adding bigdatacovid page with pagination

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Link } from "react-router";
-import BigDataCovid from "./pages/BigDataCovid";
-
 function App() {
   return (
     <>
       <h3>Landing page</h3>
       <Link to="/coviddata">Covid data</Link>
+      <br />
+      <Link to="/coviddatapagination">Covid data with pagination</Link>
       <br />
       <Link to="/wikiarticle">Wiki articles</Link>
       <br />
